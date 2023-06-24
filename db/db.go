10502f52bd7d7af534374c10e0a76a151fb6dc3d
arthur-carvalho <arthur.teixeira@objective.com.br>
2023-06-24 11:32:02 -0300

    feat: Added .env support

--- db/db.go
+++ db/db.go
@@ -3,19 +3,19 @@ package db
 import (
 	"database/sql"
 	"fmt"
-    _ "github.com/lib/pq"
-)
+	"os"
 
-const (
-    host = "db"
-    port = 5432
-    user = "postgres"
-    password = "root"
-    dbname = "angleapi"
+	_ "github.com/lib/pq"
 )
 
 func GetConn() (*sql.DB, error) {
-    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+    host := os.Getenv("DB_HOST")
+    port := os.Getenv("DB_PORT")
+    user := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    dbname := os.Getenv("DB_NAME")
+
+    psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
     db, err := sql.Open("postgres", psqlInfo)
     if err != nil {
         return nil, err
--- main.go
+++ main.go
@@ -4,12 +4,18 @@ import (
 	"clock-angle-api/controller"
 	"log"
 	"net/http"
+    "github.com/joho/godotenv"
 )
 
 func main() {
+    err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Could not load .env file")
+	}
+
     controller := controller.NewAngleController()
 	http.HandleFunc("/api/clock", controller.GetAngle)
-	err := http.ListenAndServe(":3000", nil)
+	err = http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatalln("Error starting server: ", err)
 	}
