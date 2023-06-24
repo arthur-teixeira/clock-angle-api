889696bef88b883809d4bcae7a22440eef5fa1a1
arthur-carvalho <arthur.teixeira@objective.com.br>
2023-06-24 11:16:34 -0300

    refactor: changed route

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
     controller := controller.NewAngleController()
-	http.HandleFunc("/api/v1/angle", controller.GetAngle)
+	http.HandleFunc("/api/clock", controller.GetAngle)
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatalln("Error starting server: ", err)
